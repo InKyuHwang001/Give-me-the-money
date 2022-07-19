7723edbb139740b73a43756ff74c08cf8c65d3e9
InGyu Hwang <hwanginkyu42@naver.com>
2022-07-19 19:54:55 +0900

    동생 골리기용

---
+++ givemoney/index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+let imgs=document.querySelectorAll('img');
+imgs.forEach((a,i)=>{
+    a.src = 'https://cdn.ppomppu.co.kr/zboard/data3/2019/0906/m_20190906154448_xtfpocmz.jpg'
+})
+
+}, 500)
